0f3206de0f33283f27445a4331609d31bce0d5da
Viacheslav Chykov <viacheslav.chykov@gmail.com>
2021-12-16 21:43:27 +0200

    Part 2 of first project

--- First project/js/script.js
+++ First project/js/script.js
@@ -1,45 +1,55 @@
 
 /* Задание на урок:
 
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
- 'Сколько фильмов вы уже посмотрели?'
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
 
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить путой массив
-    - private - в это свойство поместить boolean(логическое) значение false
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем польователя к вопросам опять. (К любой строке можно обратиться как 
+str.length - и поучить её длину)
  
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате:
-    movies: {
-        'logan': '8.1'
-    }
+3) При помощи условий проверить personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
 
-Проверить, чтобы все работало без ошибок в консоли */
+4) Потренироваться и переписать цикл еще двумя способами*/
 
 "use strict";
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
     private: false,
 };
 
-const lastSeenFilmName1 = prompt("Один из последних просмотренных фильмов?", ""),
-      lastSeenFilmEstimate1 = prompt("На сколько оцените его?", ""),
-      lastSeenFilmName2 = prompt("Один из последних просмотренных фильмов?", ""),
-      lastSeenFilmEstimate2 = prompt("На сколько оцените его?", "");
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
-personalMovieDB.movies[lastSeenFilmName1] =  lastSeenFilmEstimate1;
-personalMovieDB.movies[lastSeenFilmName2] =  lastSeenFilmEstimate2;
+personalMovieDB.count = numberOfFilms;
+
+let i = 2;
+while (i > 0) {
+    const lastSeenFilmName = prompt("Один из последних просмотренных фильмов?", ""),
+        lastSeenFilmEstimate = prompt("На сколько оцените его?", "");
+    if (lastSeenFilmName == null || lastSeenFilmName == "" || lastSeenFilmName.length > 50 ||
+        lastSeenFilmEstimate == null || lastSeenFilmEstimate == "") {
+            console.log("Ошибка. Вводите непустую строку до 50 символов!");
+        continue;
+    } else {
+        personalMovieDB.movies[lastSeenFilmName] = lastSeenFilmEstimate;
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10 && personalMovieDB.count >= 0) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if ((personalMovieDB.count >= 10) && (personalMovieDB.count <= 30)) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
 
 console.log(personalMovieDB);
