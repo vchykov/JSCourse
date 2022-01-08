b7f15960334aaa47df52dd40e524f4d2f56331bc
Viacheslav Chykov <viacheslav.chykov@gmail.com>
2022-01-08 15:39:28 +0200

    Create the countdown timer on the site

--- Food_dist/js/script.js
+++ Food_dist/js/script.js
@@ -1,5 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => {
-
+    // Tabs
     const tabsContent = document.querySelectorAll('.tabcontent'),
         tabs = document.querySelectorAll('.tabheader__item'),
         tabsParent = document.querySelector('.tabheader__items');
@@ -21,6 +21,9 @@ window.addEventListener('DOMContentLoaded', () => {
         tabs[i].classList.add('tabheader__item_active');
     }
 
+    hideTabsContent();
+    showTabContent();
+
     tabsParent.addEventListener('click', (event) => {
         const target = event.target;
         if (target && target.classList.contains('tabheader__item')) {
@@ -33,7 +36,57 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    hideTabsContent();
-    showTabContent();
+    // Timer
+    const deadline = '2022-01-09T00:00:00';
+
+    function getTimeRemaining(endTime) {
+        const total = Date.parse(endTime) - Date.now(),
+            days = Math.floor(total / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((total / (1000 * 60 * 60)) % 24),
+            minutes = Math.floor((total / (1000 * 60)) % 60),
+            seconds = Math.floor((total / 1000) % 60);
+
+        return {
+            total,
+            days,
+            hours,
+            minutes,
+            seconds,
+        };
+    }
+
+    function addZero(num) {
+        if (num <= 9 && num >= 0) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    function setTimer(selector, endTime) {
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds');
+              timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+        function updateClock() {
+            const  t = getTimeRemaining(endTime);
+
+            days.innerHTML = addZero(t.days);
+            hours.innerHTML = addZero(t.hours);
+            minutes.innerHTML = addZero(t.minutes);
+            seconds.innerHTML = addZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setTimer('.timer', deadline);
 
 });
