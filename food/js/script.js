1eddc1034be3a122dca2e76124f2d8949c50f29b
Viacheslav Chykov <viacheslav.chykov@gmail.com>
2022-01-18 22:57:16 +0200

    Add the interaction with JSON-server

--- food/js/script.js
+++ food/js/script.js
@@ -306,5 +306,8 @@ window.addEventListener('DOMContentLoaded', () => {
         }, 4000);
     }
 
+    fetch('http://localhost:3000/menu')
+        .then(data => data.json())
+        .then(res => console.log(res));
 
 });
