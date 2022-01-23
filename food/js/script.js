8c1a9c8f11909fbd662a546a5cf0759e6e3e7a64
Viacheslav Chykov <viacheslav.chykov@gmail.com>
2022-01-23 21:41:41 +0200

    Inplement a regexp in the script.js

--- food/js/script.js
+++ food/js/script.js
@@ -359,52 +359,49 @@ window.addEventListener('DOMContentLoaded', () => {
         if (i == 0) {
             dot.style.opacity = 1;
         }
-
         indicators.append(dot);
         dots.push(dot);
     }
 
     function updateSlider() {
         current.textContent = addZero(slideIndex);
+
+        slidesFields.style.transform = `translateX(-${offset}px)`;
             
         dots.forEach(dot => dot.style.opacity = '.5');
         dots[slideIndex - 1].style.opacity = 1;
     }
 
+    function deleteNotDigits(str) {
+        return +str.replace(/\D/g, '');
+    }
+
 
     next.addEventListener('click', () => {
-        if (offset == +width.slice(0, -2) * (slides.length - 1)) { //'500px'
+        if (offset == deleteNotDigits(width) * (slides.length - 1)) { //'500px'
             offset = 0;
         } else {
-            offset += +width.slice(0, -2);
+            offset += deleteNotDigits(width);
         }
-
-        slidesFields.style.transform = `translateX(-${offset}px)`;
-
         if (slideIndex == slides.length) {
             slideIndex = 1;
         } else {
             slideIndex++;
         }
-
         updateSlider();
     });
 
     prev.addEventListener('click', () => {
-        if (offset == 0) { //'500px'
-            offset = +width.slice(0, -2) * (slides.length - 1);
+        if (offset == 0) { //'0px'
+            offset = deleteNotDigits(width) * (slides.length - 1);
         } else {
-            offset -= +width.slice(0, -2);
+            offset -= deleteNotDigits(width);
         }
-
-        slidesFields.style.transform = `translateX(-${offset}px)`;
-
         if (slideIndex == 1) {
             slideIndex = slides.length;
         } else {
             slideIndex--;
         }
-
         updateSlider();
     });
 
@@ -413,9 +410,7 @@ window.addEventListener('DOMContentLoaded', () => {
             const slideTo = e.target.getAttribute('data-slide-to');
 
             slideIndex = slideTo;
-            offset = +width.slice(0, -2) * (slideTo - 1);
-
-            slidesFields.style.transform = `translateX(-${offset}px)`;
+            offset = deleteNotDigits(width) * (slideTo - 1);
 
             updateSlider();
         });
